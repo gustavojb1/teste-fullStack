11a892378b322b31814b9a6ea4c8f8157b0e87e6
gustavojb1 <gustavojunqueirab@gmail.com>
2024-03-07 12:02:33 -0300

    fix: brute-force and add comments

--- backend/src/business/ClientBusiness.ts
+++ backend/src/business/ClientBusiness.ts
@@ -119,9 +119,9 @@ export class ClientBusiness {
             undefined,
             undefined,
             true
-        );
+        ); // Buscar todos os clientes cadastrados no banco de dados
     
-        if (clientsDB.length < 2) {
+        if (clientsDB.length < 2) { // Verificar se há pelo menos 2 clientes cadastrados
             throw new NotFoundError("Impossível fazer o cálculo com menos de 2 clientes cadastrados");
         }
     
@@ -132,44 +132,43 @@ export class ClientBusiness {
                 x: clientDB.x,
                 y: clientDB.y
             }
-        });
+        }); // Mapear os dados dos clientes para um formato mais simples com apenas id, nome, x e y
     
-        let shortestDistance = Infinity;
-        let shortestRoute: ClientToCalculate[] = [];
+        let shortestDistance = Infinity; // Inicializar a menor distância com infinito, pois qualquer distância será menor que infinito
+        let shortestRoute: ClientToCalculate[] = []; // Inicializar a menor rota como um array vazio
     
         // Criar um objeto para representar a empresa
         let empresa: ClientToCalculate = {
-            id: 0,
+            id: -1,
             nome: "Empresa",
             x: 0,
             y: 0
-        };
+        }; // Inicializa a empresa com id -1, nome "Empresa", x 0 e y 0 (no ponto 0)
     
         // Calcular todas as permutações possíveis
-        const routes = this.permute(clientsToCalculate);
+        const routes = this.permute(clientsToCalculate); // Calcular todas as permutações(rotas) possíveis
 
-        for (const route of routes) {
-            let distance = 0;
-            let lastPoint = empresa;  // A empresa
+        for (const route of routes) { // Loop para percorrer todas as rotas possíveis
+            let distance = 0; // Inicializa a distância com 0
+            let lastPoint = empresa;  // Inicializa o último ponto com a empresa pois o teste pede que a rota comece e termine na empresa
         
-            for (const point of route) {
-                distance += this.calculateDistance(lastPoint, point);
-                lastPoint = point;
-            }
+            for (const point of route) { // Loop para percorrer todos os pontos da rota
+                distance += this.calculateDistance(lastPoint, point); // Calcular a distância entre o último ponto e o ponto atual
+                lastPoint = point; // Atualizar o último ponto com o ponto atual
+            } // Esse loop calculará a distância total da rota (soma das distâncias entre cada ponto da rota) e atualizará o último ponto com o ponto atual
         
-            distance += this.calculateDistance(lastPoint, empresa);  // Retornar à empresa
+            distance += this.calculateDistance(lastPoint, empresa);  // aqui calcula a distância entre o último ponto(soma total da distancia entre todos os pontos da rota) e a empresa
+            
         
-            if (distance < shortestDistance) {
-                shortestDistance = distance;
-                shortestRoute = [empresa].concat(route).concat(empresa);
+            if (distance < shortestDistance) { // Verificar se a distância da rota atual é menor que a menor distância encontrada até agora
+                shortestDistance = distance; // Atualizar a menor distância com a distância da rota atual
+                shortestRoute = route; // Atualizar a menor rota com a rota atual
             }
         }
-
-        const shortRoutesWithouthEmpresa = shortestRoute.filter((client) => client.id !== 0);
     
         // Buscar novamente os dados completos dos clientes
         const shortestRouteClients: Client[] = [];
-        for (const client of shortRoutesWithouthEmpresa) {
+        for (const client of shortestRoute) {
             const clientDB = clientsDB.find(c => c.id === client.id);
             if (clientDB) {
                 shortestRouteClients.push(new Client(
@@ -185,33 +184,39 @@ export class ClientBusiness {
             }
         }
 
-    
         return this.clientDTO.bruteForceOutput(shortestRouteClients);
-    
+
     }
 
 
-    private calculateDistance(point1: ClientToCalculate, point2: ClientToCalculate): number {
-        const dx = point2.x - point1.x;
-        const dy = point2.y - point1.y;
-        return Math.sqrt(dx * dx + dy * dy);
+    private calculateDistance(point1: ClientToCalculate, point2: ClientToCalculate): number { // Função para calcular a distância entre dois pontos
+        const dx = point2.x - point1.x; // Calcular a diferença entre as coordenadas x dos dois pontos
+        const dy = point2.y - point1.y; // Calcular a diferença entre as coordenadas y dos dois pontos
+        return Math.sqrt(dx * dx + dy * dy); // Calcular a distância entre os dois pontos usando o teorema de Pitágoras
+        // ou seja pitagoras é a raiz quadrada da soma dos quadrados dos catetos(cateto é a diferença entre as coordenadas x e y dos dois pontos)
     }
-    
-    private permute(array: ClientToCalculate[]): ClientToCalculate[][] {
-        if (array.length <= 1) return [array];
-        let result = [];
-        for (let i = 0; i < array.length; i++) {
-            const rest = array.slice(0, i).concat(array.slice(i + 1));
-            for (let permutation of this.permute(rest)) {
-                result.push([array[i]].concat(permutation));
+
+
+    private permute(array: ClientToCalculate[]): ClientToCalculate[][] { // Função para calcular todas as permutações(rotas) possíveis
+        if (array.length <= 1) return [array];// Se o array tiver apenas um elemento, retorna ele mesmo
+        let result = []; // Inicializa um array para armazenar as permutações(possíveis rotas)
+        const used = new Set(); // Conjunto para rastrear elementos já usados
+        for (let i = 0; i < array.length; i++) { // Loop para percorrer o array
+            if (!used.has(array[i])) { // Verifica se o elemento ainda não foi usado
+                used.add(array[i]); // Adiciona o elemento ao conjunto de elementos usados
+                const rest = array.slice(0, i).concat(array.slice(i + 1)); // Cria um novo array sem o elemento atual
+                for (let permutation of this.permute(rest)) { // Chama a função recursivamente para calcular as permutações(rotas) do novo array reduzido
+                    result.push([array[i]].concat(permutation)); // Adiciona o elemento atual no início de cada permutação(rotas) do novo array de resultado
+                }
             }
         }
-        return result;
+        return result; // Retorna todas as permutações(rotas) possíveis
+        //Esse Passo gerará uma complexidade de fatorial O(n!) para o cálculo de todas as permutações possíveis, no caso de 4 clientes, serão 24 rotas possíveis, no caso de 5 clientes, serão 120 rotas possíveis, e assim por diante.
     }
 
     
     public async heuristic(): Promise<BruteForceOutputDTO> {
-        const clientsDB: ClientDB[] = await this.clientDatabase.getClients(
+        const clientsDB: ClientDB[] = await this.clientDatabase.getClients( // Buscar todos os clientes cadastrados no banco de dados
             1,
             100,
             undefined,
@@ -221,11 +226,11 @@ export class ClientBusiness {
             true
         );
     
-        if (clientsDB.length < 2) {
+        if (clientsDB.length < 2) { // Verificar se há pelo menos 2 clientes cadastrados
             throw new NotFoundError("Impossível fazer o cálculo com menos de 2 clientes cadastrados");
         }
     
-        let clientsToCalculate: ClientToCalculate[] = clientsDB.map(clientDB => {
+        let clientsToCalculate: ClientToCalculate[] = clientsDB.map(clientDB => { // Mapear os dados dos clientes para um formato mais simples com apenas id, nome, x e y
             return {
                 id: clientDB.id,
                 nome: clientDB.nome,
@@ -234,32 +239,31 @@ export class ClientBusiness {
             }
         });
     
-        // Criar um objeto para representar a empresa
-        let empresa: ClientToCalculate = {
+        let empresa: ClientToCalculate = { // Criar um objeto para representar a empresa com id 0, nome "Empresa", x 0 e y 0
             id: 0,
             nome: "Empresa",
             x: 0,
             y: 0
         };
     
-        let route: ClientToCalculate[] = [empresa];
-        let currentPoint = empresa;
+        let route: ClientToCalculate[] = [empresa]; // Inicializar a rota com a empresa
+        let currentPoint = empresa; // Inicializar o ponto atual com a empresa para começar o cálculo
     
-        while (clientsToCalculate.length > 0) {
-            let nearestPoint = clientsToCalculate[0];
-            let shortestDistance = this.calculateDistance(currentPoint, nearestPoint);
+        while (clientsToCalculate.length > 0) { // Loop para percorrer todos os clientes
+            let nearestPoint = clientsToCalculate[0]; // Inicializar o ponto mais próximo com o primeiro cliente
+            let shortestDistance = this.calculateDistance(currentPoint, nearestPoint); // Inicializar a menor distância com a distância entre o ponto atual e o primeiro cliente
     
-            for (const point of clientsToCalculate) {
-                const distance = this.calculateDistance(currentPoint, point);
-                if (distance < shortestDistance) {
-                    nearestPoint = point;
-                    shortestDistance = distance;
+            for (const point of clientsToCalculate) { // Loop para percorrer todos os clientes
+                const distance = this.calculateDistance(currentPoint, point); // Calcular a distância entre o ponto atual e o cliente atual
+                if (distance < shortestDistance) { // Verificar se a distância é menor que a menor distância encontrada até agora
+                    nearestPoint = point; // Atualizar o ponto mais próximo com o cliente atual
+                    shortestDistance = distance; // Atualizar a menor distância com a distância atual
                 }
             }
     
-            route.push(nearestPoint);
-            clientsToCalculate = clientsToCalculate.filter(client => client.id !== nearestPoint.id);
-            currentPoint = nearestPoint;
+            route.push(nearestPoint); // Adicionar o ponto mais próximo à rota
+            clientsToCalculate = clientsToCalculate.filter(client => client.id !== nearestPoint.id); // Remover o ponto mais próximo da lista de clientes para calcular
+            currentPoint = nearestPoint; // Atualizar o ponto atual com o ponto mais próximo
         }
     
         route.push(empresa);  // Retornar à empresa
